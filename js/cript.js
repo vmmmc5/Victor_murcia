380de6dd63e18ea08744d9ba3ce00c7386485d78
Victor_Murcia <vimamucu7@gmail.com>
2021-09-14 17:10:46 -0500

    agregando funcinamiento con js

---
+++ js/cript.js
@@ -0,0 +1,28 @@
+$(document).ready(main);
+
+var contador = 0;
+
+
+function main() {
+    $(".menu-bar") .click(function() {
+        if (contador == 1){
+            $("nav").animate({
+                left: "0"
+            });
+            contador = 0;
+
+        }
+        else{
+            contador = 1;
+            $("nav").animate ({
+                left: "-100%"
+            });
+        }
+
+    });
+    $(".submenu").click(function() {
+        $(this).children(".children").slideToggle();
+
+    });
+
+}

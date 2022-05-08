74365d0628d2fb83f7ec7f9539c3ec7ff04eb7da
victor_manuel <vimamucu7@gmail.com>
2022-05-07 21:12:22 -0500

    deco index

--- interfases/js/fields.js
+++ interfases/js/fields.js
@@ -1,6 +1,10 @@
-function validarInfo(numeroDocumento){
-    var text = document.getElementById(numeroDocumento).value;
-    if (text ==="") {
+function validarInfo(numeroDocumento,venombr){
+    var doc = document.getElementById(numeroDocumento).value;
+    var nom = document.getElementById(venombr).value;
+    if (doc ==="") {
         alert("Ingrese el número de documento");
+    }
+    if (nom ==="") {
+        alert("Ingrese su nombre");
     }
 }

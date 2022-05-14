05004afaede0da4f37678c5fef288ef0f1ccb0fc
victor_manuel <vimamucu7@gmail.com>
2022-05-14 12:13:39 -0500

    netflix img y formulario

--- interfases/js/fields.js
+++ interfases/js/fields.js
@@ -1,10 +1,27 @@
-function validarInfo(numeroDocumento,venombr){
-    var doc = document.getElementById(numeroDocumento).value;
-    var nom = document.getElementById(venombr).value;
-    if (doc ==="") {
-        alert("Ingrese el número de documento");
+  function validarInfo(diacita,horacita,nombre,direc,fnac){
+    var dia = document.getElementById(diacita).value;
+    var hora = document.getElementById(horacita).value;
+    var nom = document.getElementById(nombre).value;
+    var dir = document.getElementById(direc).value;
+    var fna = document.getElementById(fnac).value;
+    if (dia.length===0) {
+        alert("Ingrese el dia de la cita");
+
     }
-    if (nom ==="") {
-        alert("Ingrese su nombre");
+    if (hora.length===0) {
+        alert("Ingrese la hora de la cita");
+       
+    }
+    if (nom.length===0) {
+        alert("Ingrese el Nombre");
+      
+    }
+    if (dir.length===0) {
+        alert("Ingrese la dirección");
+    
+    }
+    if (fna.length===0) {
+        alert("Ingrese la fecha de nacimiento");
+    
     }
 }

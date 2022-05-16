0438411ca73d3d879f2a58ceaffac4a3b4be6330
victorino <vimamucu7@gmail.com>
2022-05-16 11:51:49 -0500

    carrito

---
+++ interfases/carritocompras/js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+function pruebaSuma() {
+    let cantidadLibros = document.getElementById("cantidadLibros").innerHTML;
+    if (cantidadLibros < 10) {
+        cantidadLibros =Number(cantidadLibros) + 1;
+        document.getElementById("cantidadLibros").innerHTML = cantidadLibros;
+    }    
+}
+function pruebaResta() {
+    let cantidadLibros = document.getElementById("cantidadLibros").innerHTML;
+    if (cantidadLibros > 0 ) {
+        cantidadLibros = Number(cantidadLibros) - 1;
+        document.getElementById("cantidadLibros").innerHTML = cantidadLibros;
+    }
+
+    
+
+    
+}

3075250539ea1cc0b3e6e99c08cc0efa90c03a54
victorino <vimamucu7@gmail.com>
2022-05-23 08:47:04 -0500

    sumar stock

--- interfases/carritocompras/js/script.js
+++ interfases/carritocompras/js/script.js
@@ -1,15 +1,33 @@
 "use strict"
+let cantidadLibrosGlobal = document.getElementById("cantidadLibros").innerHTML;
+
 function pruebaSuma() {
-    let cantidadLibros = document.getElementById("cantidadLibros").innerHTML;
-    if (cantidadLibros < 10) {
-        cantidadLibros =Number(cantidadLibros) + 1;
-        document.getElementById("cantidadLibros").innerHTML = cantidadLibros;
+    if (cantidadLibrosGlobal < 10) {
+        cantidadLibrosGlobal =Number(cantidadLibrosGlobal) + 1;
+        document.getElementById("cantidadLibros").innerHTML = cantidadLibrosGlobal;
     }    
+    modificarTotal();
 }
 function pruebaResta() {
-    let cantidadLibros = document.getElementById("cantidadLibros").innerHTML;
-    if (cantidadLibros > 0 ) {
-        cantidadLibros = Number(cantidadLibros) - 1;
-        document.getElementById("cantidadLibros").innerHTML = cantidadLibros;
+    cantidadLibrosGlobal = document.getElementById("cantidadLibros").innerHTML;
+    if (cantidadLibrosGlobal > 0 ) {
+        cantidadLibrosGlobal = Number(cantidadLibrosGlobal) - 1;
+        document.getElementById("cantidadLibros").innerHTML = cantidadLibrosGlobal;
     }  
+    modificarTotal();
 }
+function modificarTotal() {
+    let cantidadLibros = document.getElementById("cantidadLibros").innerHTML;
+    if (cantidadLibros == "1") {
+        document.getElementById("cantidadSingularPlural").innerHTML = cantidadLibros + " producto";
+    } else {
+        document.getElementById("cantidadSingularPlural").innerHTML = cantidadLibros + " productos";
+    }
+    mostrarTotal();
+}
+function mostrarTotal() {
+    let valorUnitario = document.getElementById("valorUnitario").innerHTML;
+    let costoTotal = Number(valorUnitario) * Number(cantidadLibrosGlobal);
+    document.getElementById("costoTotal").innerHTML = costoTotal;
+}
+
